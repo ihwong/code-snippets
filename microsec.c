b5697604276910b3d6e1fd329d756c8cc8a31a7f
Hoyoung Kim <trig4800@gmail.com>
2020-01-30 20:27:53 +0900

    C code snippet (current time in microseconds)
    C function that prints current time in microseconds (ex. 20:19:47.030744)

---
+++ microsec.c
@@ -0,0 +1,34 @@
+/*
+  C programming - current time in microseconds
+  $ ./microsec
+  20:19:47.030744
+*/
+
+#include <stdio.h>
+#include <time.h>
+#include <sys/time.h>
+
+// printt() requires <time.h> and <sys/time.h>
+void printt(void);
+
+int main(int argc, char *argv[]) {
+  printt();
+  return 0;
+}
+
+void printt(void) {
+  
+  struct timeval tv;
+  struct tm *lt;
+  time_t t = gettimeofday(&tv, NULL);
+  lt = localtime(&tv.tv_sec);
+  
+  if (t == -1 || lt == NULL) {
+    printf("CURRENT TIME UNKNOWN ");
+    return;
+  }
+  
+  printf("%02d:%02d:%02d.%06ld", lt->tm_hour, lt->tm_min, lt->tm_sec, tv.tv_usec);
+  return;
+  
+}
